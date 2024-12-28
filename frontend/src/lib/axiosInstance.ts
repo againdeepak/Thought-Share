43af4532dc51649cb14e36716b86d44f0de40152
Deepak <deepakkumar09092003@gmail.com>
2024-12-28 22:05:57 +0530

    add url to client

--- frontend/src/lib/axiosInstance.ts
+++ frontend/src/lib/axiosInstance.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000", // Centralized base URL
+  baseURL: "https://thought-share-lpks.onrender.com/", // Centralized base URL
   // timeout: 5000, // Optional timeout
   headers: { 'Content-Type': 'application/json' } // Default headers
 });
